1185d2bf5ab4bea3d577217b30730f26c9d32ccf
Leslie Stevens <flylady@mail.utexas.edu>
2019-01-21 20:56:09 +0000

    build login route and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,18 @@
 class UsersController < ApplicationController
 
-  get '/login' do
-    erb :login
+  get '/login' do #render the login form
+
+    erb :'/users/login'
   end
 
   post '/login' do
-
+    #receives login form
+    #finds the user
+    #logins user in (creates a session)
   end
 
   get '/signup' do
-    
+
   end
 
 end
