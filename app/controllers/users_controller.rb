b5610551105bb6b01714ba4f08b3913363a401dd
Leslie Stevens <flylady@mail.utexas.edu>
2019-01-22 22:55:18 +0000

    added submit value to login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,26 @@ class UsersController < ApplicationController
   post '/login' do
     #receives login form
     #finds the user
-
+    #binding.pry
     @user = User.find_by(email: params[:email])
+    binding.pry
     if @user.authenticate(params[:password])
-      session[:user_id] = @user.idea   #logins user in (creates a session)
+      session[:user_id] = @user.id   #logins user in (creates a session)
       redirect "users/#{@user.id}"
+    else
+      #invalid credentials message
+      #redirect to login page
+    end
 
   end
 
   get '/signup' do
 
   end
+
+  get 'users/:id' do
+    "user's show page"
+  end
+
 
 end
