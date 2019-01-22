eae64895415d8c0ff3e10e82df23461c25f841ec
Leslie Stevens <flylady@mail.utexas.edu>
2019-01-22 22:30:34 +0000

    build helper methods logged_in? and current_user in app_con

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,20 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "keep_a_secret"
   end
 
-  get "/" do
+  get '/' do
     erb :welcome
+  end
+
+  helpers do
+
+    def logged_in?
+      !! current_user
+    end
+
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
+
   end
 
 end
