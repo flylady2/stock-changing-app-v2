981c829860f17ccd28cfb873a9b183ecd9e6b63c
Leslie Stevens <flylady@mail.utexas.edu>
2019-01-25 00:57:56 +0000

    edit signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,24 +15,27 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id   #logins user in (creates a session)
       redirect "users/#{@user.id}"
     else
+      redirect '/login' #need to add failure message
       #invalid credentials message
       #redirect to login page
     end
-
   end
 
   get '/signup' do
     erb :'/users/signup'
   end
 
-  post '/signup' do
+  post '/users' do
 
     #binding.pry
     #create new user and persist them to the db
     user = User.new(params)
     if user.name != "" && user.email != "" && user.password != ""
       user.save
+      session[:user_id] = user.id #logging new user in
       redirect "/users/#{user.id}"
+    else
+      redirect 'signup' #need to add a failure message
     end
     #binding.pry
     #if params
@@ -42,6 +45,12 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
     erb :'users/show'
   end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
+
 
 
 end
