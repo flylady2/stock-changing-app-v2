17e1812a2b725fa11025a3a728c3c95182851bb6
Leslie Stevens <flylady@mail.utexas.edu>
2019-01-28 19:17:11 +0000

    build route to create new stock boxes

--- app/controllers/stock_boxes_controller.rb
+++ app/controllers/stock_boxes_controller.rb
@@ -11,4 +11,20 @@ class StockBoxesController < ApplicationController
     end
   end
 
+  post '/stock_boxes' do
+    if !logged_in?
+      redirect '/login'
+    end
+    if params[:name] != ""
+      #binding.pry
+      @stock_box = StockBox.new(params)
+      @stock_box.user_id = current_user.id
+      @stock_box.save
+      binding.pry
+    else
+      redirect 'stock_boxes/new'
+      #add failure message
+    end
+  end
+
 end
