c24035024ed277590c082efe0f0141852c8272f6
Leslie Stevens <flylady@mail.utexas.edu>
2019-02-23 23:01:17 +0000

    add validation for email uniqueness to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :email, :password
+  validates :email, uniqueness: true
   belongs_to :lab
   has_many :stock_boxes
   has_many :stocks, through: :stock_boxes
